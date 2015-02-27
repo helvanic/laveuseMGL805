e66561c138058b53d4f38734dce45352c729f907
Joffrey <joffrey.coulon@gmail.com>
2015-02-27 18:56:09 -0500

    Petite correction au niveau du formalisme

---
+++ Laveuse/src/laveuse/FinalVariables.java
@@ -0,0 +1,14 @@
+package laveuse;
+
+public class FinalVariables {
+	
+	public final String imposs = "Impossible";
+	public final String a = "Impossible";
+	public final String b = "Impossible";
+	public final String c = "Impossible";
+	public final String d = "Impossible";
+	public final String e = "Impossible";
+	public final String f = "Impossible";
+	public final String g = "Impossible";
+
+}
